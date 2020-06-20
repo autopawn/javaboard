485968d3c9b23d932837bc64fc75b9080c46a5ee
ppquezada <64628068+ppquezada@users.noreply.github.com>
2020-06-20 18:18:25 -0400

    Add files via upload

--- src/fivefieldkono/FiveFieldKono.java
+++ src/fivefieldkono/FiveFieldKono.java
@@ -24,7 +24,7 @@ public class FiveFieldKono extends GridGame {
         size_x = 5;
         size_y = 5;
         pieces = new LinkedList<Piece>();
-
+        this.game_name = "FiveFieldKono";
         // Create a player 0 piece on player 0 starting positions
         for(int[] ps : base0){
             pieces.add(new KonoPiece(0,ps[0],ps[1]));
