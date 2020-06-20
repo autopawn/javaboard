4d3801d9fb46373f9f3a357eaeba269d561cd244
Yi-Zhou-Zhou <65474467+Yi-Zhou-Zhou@users.noreply.github.com>
2020-06-20 15:38:48 -0400

    Update Play.java
    Added instructions

--- src/simplewars/Play.java
+++ src/simplewars/Play.java
@@ -14,7 +14,17 @@ public class Play {
         
         while(true){
             Scanner in = new Scanner(System.in);
-            System.out.println("Elija el bando: \n\n1. Zombie\n\n2. Soldier");  
+            while(true){
+                System.out.println("\n- Jugar                   - Instrucciones");
+                String comm = in.next();
+                if(comm.equals("Jugar")){
+                    break;
+                }
+                else{
+                    System.out.println("\nSimple Wars es un juego en el que cada equipo cuenta con 3 soldados, estas unidades son capaces de moverse hasta un máximo de 4 casillas y sólamente con movimientos verticales y horizontales, notar que un movimiento es inválido si: \n 1.- No está en rango \n 2.- Hay un enemigo en la casilla \n 3.- La casilla excede el tamaño del tablero.\n\n El objetivo del juego es dañar a las unidades enemigas, al mover tus piezas en una casilla adyacente a una pieza rival les quitarás 1 de HP, gana quien deje al equipo rival sin piezas restantes. ¡Ten en consideración que cada pieza tiene 3 de HP!");
+                }
+            }
+            System.out.println("\nElija el bando: \n\n1. Zombie\n\n2. Soldier");  
             String bando = in.next();
             if(bando.equals("Zombie") || bando.equals("Soldier")){ // check for equality, not identity
                 player_choice = bando;
