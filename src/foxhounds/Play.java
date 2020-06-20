0a9997a4716fb8db1bfd4ec8f7cc76ca32b42684
ppquezada <64628068+ppquezada@users.noreply.github.com>
2020-06-20 18:05:35 -0400

    Add files via upload

--- src/foxhounds/Play.java
+++ src/foxhounds/Play.java
@@ -1,19 +1,28 @@
 package foxhounds;
 
+import javaboard.ExecutorRecorder;
 import javaboard.Executor;
 import javaboard.Player;
 import javaboard.PlayerCPUEval;
 import javaboard.PlayerCPURandom;
+import javaboard.PlayerHumanTerminal;
+import java.util.*;
 
 public class Play {
 
     // Play a FoxAndHounds game using the Executor
     public static void main(String[] args) {
+        String player_name;//Aqui es donde se pregunta por el nombre del player(en caso de que sea humano)
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter players name: ");
+        player_name = sc.nextLine();
+
         FoxAndHounds game = new FoxAndHounds();
 
-        Player[] players = {new PlayerCPURandom(), new PlayerCPUEval()};
+        Player[] players = {new PlayerHumanTerminal(player_name) , new PlayerCPUEval()};
 
-        Executor exec = new Executor();
+        ExecutorRecorder exec = new ExecutorRecorder();
         exec.runGame(game,players);
     }
 }
