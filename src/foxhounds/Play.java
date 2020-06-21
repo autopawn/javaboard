4b0f15e2bd3039c07f950574b0757bccfebc0826
darinkaqt <darinkaqt@DESKTOP-2LNR1R0.localdomain>
2020-06-21 13:48:22 -0400

    historygamesdq

--- src/foxhounds/Play.java
+++ src/foxhounds/Play.java
@@ -1,6 +1,7 @@
 package foxhounds;
 
 import javaboard.Executor;
+import javaboard.ExecutorRecorder;
 import javaboard.Player;
 import javaboard.PlayerCPUEval;
 import javaboard.PlayerCPURandom;
@@ -13,7 +14,14 @@ public class Play {
 
         Player[] players = {new PlayerCPURandom(), new PlayerCPUEval()};
 
+        // Builder ExecutorRecorder, with game name and array of players
+        ExecutorRecorder game_history = new ExecutorRecorder("FoxAndHounds", players);
+
         Executor exec = new Executor();
-        exec.runGame(game,players);
+        // The winning variable is saved
+        Integer winner = exec.runGame(game,players);
+
+        // Game history with game winner
+        game_history.history(winner);
     }
 }
